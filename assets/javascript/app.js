e8156c914f73f2e32e01548c719120ef6b3d0283
georgejincy <jincydon101@gmail.com>
2017-01-12 09:41:20 -0500

    Added logic to calculate next train time and minutes to arrival

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,6 +16,9 @@ var databaseRef = firebase.database();
 var trainSchedRef = firebase.database().ref().child('trainSchedTable');
 
 moment().format();
+
+var militaryFormat = 'HH:mm';
+
 //--------------------------------------------------------------
 //--------------------FUNCTIONS---------------------------------
 
@@ -31,20 +34,80 @@ function trainHtmlFromDtb(trainRowSnapshot){
 	return html;
 
 }
+
 //--------------------------------------------------------------
 
 // Load all the old as well as new rows on the train Schedule table and display on HTML
 trainSchedRef.on("child_added", function(snapshot) {
 
 	//Log current time
-	var now = moment();
-	console.log(now);
+	var currentTime = moment();
+	console.log("Current time in military time: " + currentTime.format('HH:mm'));
+
+	//Testing and debugging. testTime in military time
+	var testTime = moment('10:00', militaryFormat);
+	console.log("Test time in military format: " + testTime.format('HH:mm'));
+
+	//Variable declarations
+	var firstTrainDep = moment(snapshot.val().firstTrainTime, militaryFormat);
+	console.log("First train departure for "+ snapshot.val().trainName +" in military format: " + firstTrainDep.format('HH:mm'))
+	var nextTrainTime;
+	var freqRemainder;
+	var minToArrival;
+	var diffInMinutes;
+
+
+	//If the First train time is in the future, display next train time as first train time
+	//Else Calculate the next train time
+	if (firstTrainDep.isAfter(currentTime)){
+
+		console.log("First train time is in future");
+
+		//Next train times is same as first train time
+		var nextTrainTime = firstTrainDep;
+		console.log("Next train time for " + snapshot.val().trainName + ": " + nextTrainTime.format('HH:mm'));
+
+		//Minutes to next train
+		minToArrival = nextTrainTime.diff(currentTime, 'minutes');
+		console.log("Minutes to next " + snapshot.val().trainName + ": " + minToArrival);
+
+	}else {
+		console.log("Test time is in past");
+
+		//Calculate difference between times in minutes
+		diffInMinutes = currentTime.diff(firstTrainDep,'minutes');
+		console.log("Difference in minutes for " + snapshot.val().trainName + ": " +diffInMinutes);
+
+		//Calculate the remainder in frequency
+		freqRemainder = diffInMinutes % snapshot.val().frequency;
+		console.log("Frequency remainder: " + freqRemainder);
+
+		//Calculate Minutes to next train
+		minToArrival = snapshot.val().frequency - freqRemainder;
+		console.log("Minutes to next " + snapshot.val().trainName + ": " + minToArrival);
+
+		//Next Train time 
+		nextTrainTime = currentTime.add(minToArrival, 'minutes');
+		console.log("Next train time for " + snapshot.val().trainName + ": " + nextTrainTime.format('HH:mm'));
+
+	}
 
 	//Log snapshot
 	console.log("| Added: " + snapshot.val());
 
+	//Calculate the next train time and minutes to next train
+
+	var html = '';
+
+	html += "<tr><td>" + snapshot.val().trainName + "</td>";
+	html += "<td>" + snapshot.val().destination + "</td>";
+	html += "<td>" + snapshot.val().frequency + "</td>";
+	html += "<td>" + nextTrainTime.format('hh:mm A') + "</td>";
+	html += "<td>" + minToArrival + "</td></tr>";
+
+
 	// Add a new row to the Current train schedule table
-	 $("#train-table > tbody:last-child").append(trainHtmlFromDtb(snapshot.val()));
+	 $("#train-table > tbody:last-child").append(html);
  });
 
 
@@ -62,18 +125,24 @@ $("#submit-traininfo").on("click", function(event) {
   var firstTrainTime = $("#traintime").val().trim();
   var frequency = $("#freq").val().trim();
 
-  //log
-  console.log("Train Name: " + trainName + " | Destination: " + destination + " | FirstTrain : " + traintime + " | Frequency: " + frequency);
+  //Store inputs in a train object
+  var train = {
+  			trainName: trainName,
+  			destination: destination,
+  			firstTrainTime: firstTrainTime,
+  			frequency: frequency
+  			};
 
-   // Save the new price in Firebase
-    trainSchedRef.push({
-        "trainName": trainName,
-        "destination": destination,
-        "firstTrain": firstTrainTime,
-        "frequency": frequency
-    });
+
+  //log
+  console.log("Train Name: " + train.trainName + " | Destination: " + train.destination + " | FirstTrain : " + train.firstTrainTime + " | Frequency: " + train.frequency);
+
+   // Save the train details to Firebase
+    trainSchedRef.push(train);
 
     //Reset the Add train form
     $("#addtrain")[0].reset();
   
+    //Don't refresh the page
+  	return false;
 });
